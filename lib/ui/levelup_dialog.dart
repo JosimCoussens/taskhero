706da1ba12664a10a3aa27897b59ee57e019367a
JosimCoussens <coussensjosim@yahoo.com>
2025-05-20 19:41:42 +0200

    Level up dialog now displaying unlocked rewards

--- lib/ui/levelup_dialog.dart
+++ lib/ui/levelup_dialog.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:taskhero/core/classes/item.dart';
 import 'package:taskhero/core/constants.dart';
+import 'package:taskhero/data/shop/item_service.dart';
 
 Dialog levelUpDialog() {
-  final reachedLevel = AppParams.level.value.toString();
+  final reachedLevel = AppParams.level.value;
+  final unlockedItems = ItemService.getAtLevel(reachedLevel);
   return Dialog(
     backgroundColor: Colors.transparent,
     child: Container(
@@ -26,6 +29,7 @@ Dialog levelUpDialog() {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          spacing: AppParams.generalSpacing,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
@@ -37,13 +41,14 @@ Dialog levelUpDialog() {
                 color: Colors.white,
               ),
             ),
-            const SizedBox(height: 20),
             Text(
               'Congratulations on reaching level $reachedLevel!',
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 16, color: Colors.white70),
             ),
-            const SizedBox(height: 30),
+            unlockedItems.isNotEmpty
+                ? _showRewards(unlockedItems)
+                : _noRewardsText(),
             FloatingActionButton(
               backgroundColor: AppColors.primaryLight,
               child: const Icon(Icons.check, color: Colors.white),
@@ -55,5 +60,35 @@ Dialog levelUpDialog() {
         ),
       ),
     ),
+  );
+}
+
+Column _showRewards(List<Item> unlockedItems) {
+  return Column(
+    spacing: AppParams.generalSpacing / 2,
+    children: [
+      const Text(
+        'You have unlocked new rewards:',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 16, color: Colors.white70),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:
+            unlockedItems
+                .map(
+                  (item) => Image.asset(item.imagePath, width: 50, height: 50),
+                )
+                .toList(),
+      ),
+    ],
+  );
+}
+
+Text _noRewardsText() {
+  return const Text(
+    'No new rewards unlocked at this level.',
+    textAlign: TextAlign.center,
+    style: TextStyle(fontSize: 16, color: Colors.white70),
   );
 }

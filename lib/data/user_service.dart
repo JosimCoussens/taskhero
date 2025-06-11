804e3cc4ffc022949fe95c85f82c9197f36c00f6
JosimCoussens <coussensjosim@yahoo.com>
2025-06-11 18:54:21 +0200

    Add user existence check before creating a new user document with default values

--- lib/data/user_service.dart
+++ lib/data/user_service.dart
@@ -3,6 +3,13 @@ import 'package:taskhero/core/constants.dart';
 
 class UserService {
   static Future<void> createUser(String userUid) async {
+    // Check if the user already exists
+    final userDoc =
+        await FirebaseFirestore.instance.collection('users').doc(userUid).get();
+    if (userDoc.exists) {
+      return;
+    }
+    // Create a new user document with default values
     await FirebaseFirestore.instance.collection('users').doc(userUid).set({
       'xp': 0,
       'level': 0,
